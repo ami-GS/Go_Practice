bedcf194b9d1e6b6e9a2a8cde964682ae2ea4a6d
Daiki Aminaka <1991.daiki@gmail.com>
2014-09-20 22:06:28 +0900

    added traversal functions

--- binTree.go
+++ binTree.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 type Tree struct {
@@ -26,30 +27,80 @@ func (t *Tree) Set(v int) {
 }
 
 func (t *Tree) Find(v int) bool {
-	if t.Right == nil && t.Left == nil {
-		return false
-	}
 	if v == t.Value {
 		return true
 	} else {
 		if v > t.Value {
+			if t.Right == nil {
+				return false
+			}
 			fmt.Print(string('R'))
 			return t.Right.Find(v)
 		} else {
+			if t.Left == nil {
+				return false
+			}
 			fmt.Print(string('L'))
 			return t.Left.Find(v)
 		}
 	}
-	return false
+	//return false
+}
+
+func (t *Tree) Traversal_pre() {
+	fmt.Println(t.Value)
+	if t.Left != nil {
+		t.Left.Traversal_pre()
+	}
+	if t.Right != nil {
+		t.Right.Traversal_pre()
+	}
+}
+
+func (t *Tree) Traversal_post() {
+	if t.Left == nil && t.Right == nil {
+		fmt.Println(t.Value)
+	}
+	if t.Left != nil {
+		t.Left.Traversal_post()
+	}
+	if t.Right != nil {
+		t.Right.Traversal_post()
+	}
+	if t.Left != nil || t.Right != nil {
+		fmt.Println(t.Value)
+	}
+}
+
+func (t *Tree) Traversal_in() {
+	if t.Left == nil && t.Right == nil {
+		fmt.Println(t.Value)
+	}
+	if t.Left != nil {
+		t.Left.Traversal_in()
+		fmt.Println(t.Value)
+	}
+	if t.Right != nil {
+		t.Right.Traversal_in()
+		if t.Left == nil {
+			fmt.Println(t.Value)
+		}
+	}
 }
 
 func main() {
 	root := Tree{nil, nil, 5}
-	for i := 0; i < 10; i++ {
-		root.Set(i)
+	randSeq := rand.Perm(10)
+	fmt.Println(randSeq)
+	for _, v := range randSeq {
+		root.Set(v)
 	}
+
 	for i := 0; i < 15; i++ {
 		fmt.Println(i)
 		fmt.Println(root.Find(i))
 	}
+	root.Traversal_pre()
+	root.Traversal_post()
+	root.Traversal_in()
 }
